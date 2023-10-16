b0df7a39c13aaa6d81e94b6fc05fb631aa0e64d9
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-10-16 11:53:46 -0700

    practice scripts.js
    Co-authored-by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// function saySomething(whatToSay) {
+//     window.alert(whatToSay);
+// }
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
+
+
+// saySomething("The sum is " + add(3, 5) + ".");
