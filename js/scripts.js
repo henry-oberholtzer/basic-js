05ffd05bf375af7c269d80c70b8a9762d9508093
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-10-16 13:31:56 -0700

    more script.js function practice
    Co-authored-by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
-// function saySomething(whatToSay) {
-//     window.alert(whatToSay);
-// }
-
 function add(number1, number2) {
     return number1 + number2;
+}
+
+function sub(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+// window.alert("Your results are as follows: Adding " + number1 + " and " + number2 + " gives " + add(number1, number2) + ". Subtracting " + number2 + " from " + number1 + " gives " + sub(number1, number2) + ". Multiplying " + number1 + " by " + number2 + " gives " + multiply(number1, number2)
+// + ". Dividing " + number1 + " by " + number2 + " gives " + divide(number1, number2) + ".");
 
-
-// saySomething("The sum is " + add(3, 5) + ".");
+window.alert(
+    number1 + " + " + number2 + " = " + add(number1, number2) + ", " 
+    + number1 + " - " + number2 + " = " + sub(number1, number2) + ", " 
+    + number1 + " * " + number2 + " = " + multiply(number1, number2) + ", " + number1 + " / " + number2 + " = " + divide(number1, number2) + ".");
